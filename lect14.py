98ce5eb940ee1ed79e38c4cb76224e944f001623
Swapn Gupta <b22ch038@iitj.ac.in>
2025-07-06 05:27:15 +0530

    6 july

---
+++ lect14.py
@@ -0,0 +1,57 @@
+#  selection sort 
+
+# arr = list(map(int, input().split()))
+# print(arr)
+
+# def swap(arr , i ,j):
+#     arr[i],arr[j] = arr[j] , arr[i]
+
+# def selection_sort(arr):
+#     for i in range(len(arr)-1):
+#         mini  = i
+#         for j in range(i,len(arr)):
+#             if arr[j] < arr[mini]:
+#                 mini = j
+#         swap(arr,i,mini)
+#     return arr
+
+# print(selection_sort(arr))
+        
+# # #  Bubble Sort 
+
+# def swap(arr , i ,j):
+#     arr[i],arr[j] = arr[j] , arr[i]
+
+# arr = list(map(int,input().split()))
+# print(arr)
+
+# def bubble_sort(arr):
+#     for i in range(len(arr)-1, 0,-1):
+#         didswap = 0
+#         for j in range(0, i):
+#             if arr[j] > arr[j+1]:
+#                 swap(arr,j,j+1)
+#                 didswapn = 1
+#         if didswapn ==0:
+#             break
+#     return arr
+
+# print(bubble_sort(arr))
+
+# Insertion Sort 
+
+# arr = list(map(int, input().split()))
+# print(arr)
+
+# def swap(arr,i,j):
+#     arr[i], arr[j] = arr[j] , arr[i]
+
+# def insertion_sort(arr):
+#     for i in range(len(arr)):
+#         j = i
+#         while(j>0 and arr[j-1] > arr[j]):
+#             swap(arr,j,j-1)
+#             j -=1
+#     return arr
+
+# print(insertion_sort(arr))
