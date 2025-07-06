4f4d214fc1db086613b15ef475ae99816a8919b3
Swapn Gupta <b22ch038@iitj.ac.in>
2025-07-07 02:03:48 +0530

    done'

--- lect17.py
+++ lect17.py
@@ -12,17 +12,70 @@
 # print(largest(arr))
 
 
-#  second largest element in the array without sorting
+# #  second largest element in the array without sorting
 
-def second_largest(arr):
-    largest = arr[0]
-    second_largest = None
-    for i in range(len(arr)):
-        if arr[i] > largest:
-            second_largest = largest
-            largest = arr[i]
-    return second_largest
+# def second_largest(arr):
+#     largest = arr[0]
+#     second_largest = None
+#     if len(arr) < 2:
+#         return None  # Not enough elements
+#     for i in range(len(arr)):
+#         if arr[i] > largest:
+#             second_largest = largest
+#             largest = arr[i]
+#         elif arr[i] < largest and arr[i] > second_largest:
+#             second_largest = arr[i]
+#     return second_largest
 
-arr = list(map(int, input().split()))
-print(second_largest(arr))
+# arr = list(map(int, input().split()))
+# print(second_largest(arr))
             
+
+# #  check if an array is sorted or not
+
+# def is_sorted(arr):
+#     for i in range(1,len(arr)):
+#         if arr[i-1] <= arr[i]:
+#             continue
+#         else:
+#             return False
+#     return True
+
+
+# arr = list(map(int, input().split()))
+# print(is_sorted(arr))
+
+
+# #  remove duplicates from an unsorted array using set 
+
+# def remove_duplicates(arr):
+#     unique = set(arr)
+#     return list(unique)
+
+
+# arr = list(map(int, input().split()))
+# print(len(remove_duplicates(arr)))
+# print(remove_duplicates(arr))
+
+
+
+#  remove duplicates from sorted array using two pointers ( optimal solution )
+
+
+# def remove_duplivates(arr):
+#     if len(arr) ==0:
+#         return 0
+#     j = 0 
+#     for i in range(1, len(arr)):
+#         if arr[j] != arr[i]:
+#             j+=1
+#         arr[j] = arr[i]
+#     return j+1
+
+# arr = list(map(int, input().split()))
+# print(remove_duplivates(arr))
+
+
+
+
+
